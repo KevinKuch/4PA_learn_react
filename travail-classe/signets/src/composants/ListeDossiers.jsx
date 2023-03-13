052a0874324262b73be1fff602a710fc7943d28c
Kevin Kuch <kuchkevin96@gmail.com>
2023-03-13 15:31:04 -0400

    correction sur listdossier

--- travail-classe/signets/src/composants/ListeDossiers.jsx
+++ travail-classe/signets/src/composants/ListeDossiers.jsx
@@ -31,18 +31,21 @@ export default function ListeDossiers({dossiers, setDossiers}) {
   }
 
   return (
-    <>
       <section className="ListeDossiers">
         {
-          dossiers.map( 
-            // Remarquez l'utilisation du "spread operator" pour "étaler" les 
-            // propriétés de l'objet 'dossier' reçu en paramètre de la fonction
-            // fléchée dans les props du composant 'Dossier' !!
-            // On en parle en classe ;-)
-            dossier =>  <Dossier key={dossier.id} {...dossier} supprimerDossier={supprimerDossier} modifierDossier={modifierDossier} />
-          )
+          // Si on a des dossiers ...
+          dossiers.length > 0 ?
+            // On les affiche :
+            dossiers.map( 
+              // Remarquez l'utilisation du "spread operator" pour "étaler" les 
+              // propriétés de l'objet 'dossier' reçu en paramètre de la fonction
+              // fléchée dans les props du composant 'Dossier' !!
+              // On en parle en classe ;-)
+              dossier =>  <Dossier key={dossier.id} {...dossier} supprimerDossier={supprimerDossier} modifierDossier={modifierDossier} />
+            )
+          // et sinon, on affiche un message ...
+          : <div className='aucun-dossier'>Aucun dossier</div>
         }
       </section>
-    </>
   );
 }
